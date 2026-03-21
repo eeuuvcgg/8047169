ffa1893414d787375affd991259027f05cdbfb86
eeuuvcgg <115527005+eeuuvcgg@users.noreply.github.com>
2026-03-21 22:51:57 +0800

    Update index.config.js

--- index.config.js
+++ index.config.js
@@ -71,7 +71,7 @@ var index_config_default = {
     autoPush: true
   },
  t4: {list: [
-    {name: "✈️关注TG频道@stymei",
+    {name: "✈️点我换线路",
       address: "http://zhangqun1818.serv00.net:6628/?spider=独播库"},
     {name: "📺天下丨IPTV丨电视直播",
       address: "https://t4.jsnzkpg.uk"},
